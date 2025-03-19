1ae11ff6f7880cce0b6051c006d14b5386248557
Marcos-Sobral <vini15oficial@gmail.com>
2025-03-19 15:59:37 -0300

    Add API endpoints for retrieving donated books and adding new books

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request;
+from flask import Flask, request, jsonify;
 
 import sqlite3 
 
@@ -22,24 +22,61 @@ def mensagem_do_calote():
 
 
 def init_db():
-    #Conecte o sqlite3 no arquvio banco_dados.db com a variável conn (connection)
-    with sqlite3.connect("banco_dados.db") as conn:
+    #Conecte o sqlite3 no arquvio database.db com a variável conn (connection)
+    with sqlite3.connect("database.db") as conn:
         conn.execute("""
         create table if not exists livros(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             titulo TEXT NOT NULL,
             categoria TEXT NOT NULL,
             autor TEXT NOT NULL,
-            imagem_url TEXT NOT NULL
+            image_url TEXT NOT NULL
         )
 """)
 
 init_db()
 
-@app.route("/doar", methods=["POST"])
+@app.route("/livrosdoados", methods=["GET"])
+def livrosdoados():
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.execute(f"""
+        SELECT titulo, categoria, autor, image_url FROM livros;
+""")
+    livros = cursor.fetchall()
+    
+    # Verificando o tipo de 'livros' no console
+    print(type(livros))
 
+    for livro in livros:
+        print(", ".join(map(str, livro)))
+
+    return [
+        {"titulo": livro[0], "categoria": livro[1], "autor": livro[2], "image_url": livro[3]}
+        for livro in livros
+    ]
+
+@app.route("/doar", methods=["POST"])
 def doar():
     dados = request.get_json()
+
+    titulo = dados.get("titulo")
+    categoria = dados.get("categoria")
+    autor = dados.get("autor")
+    image_url = dados.get("image_url")
+   
+    if not titulo or not categoria or not autor or not image_url:
+        return jsonify({"Erro":"Todos os campos são obrigatórios"}), 400
+   
+    with sqlite3.connect("database.db") as conn:
+        conn.execute(f"""
+        INSERT INTO livros (titulo, categoria, autor, image_url)
+        VALUES ("{titulo}", "{categoria}", "{autor}", "{image_url}")
+""")
+
+    conn.commit() #salvar no banco
+
+    return jsonify({"mensagem":"Livro Cadastrado com sucesso"}),201
+
 
 #app.run()
 
